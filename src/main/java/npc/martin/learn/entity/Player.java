56fdda08c40ec8d9326ec044ba27022d7aa18c35
Bikathi Martin <bikathimartin@outlook.com>
2024-02-06 12:44:09 +0300

    Create missing constuctors

--- src/main/java/npc/martin/learn/entity/Player.java
+++ src/main/java/npc/martin/learn/entity/Player.java
@@ -6,6 +6,14 @@ public class Player {
     private String name;
     private int age;
 
+    public Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Player() {
+    }
+
     public String getName() {
         return name;
     }
